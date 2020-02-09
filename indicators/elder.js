32daef10bcb16f2ff7e0db3e046e9eb8eb856788
Athul A <59114880+tiger1016@users.noreply.github.com>
2020-02-09 02:46:57 -0800

    Minute today (#2)
    * improve the elder colors in spy_elder
    
    * complete npm -- --symbol=''.... commmand

--- indicators/elder.js
+++ indicators/elder.js
@@ -15,6 +15,10 @@ function getPriceBarColor({ currentEMA, prevEMA, currentMACD, prevMACD }) {
     return color;
 }
 
+const timeStamp_day = time => 
+    time.toUTCString().slice(0, 17);
+
+
 module.exports = {
     insertData: async (data, symbol) => {
         return await insertData({ data, modelName: `${symbol}_elder`, type: 'elder' });
@@ -43,46 +47,106 @@ module.exports = {
             return dbRows;
         },
         db: async symbol => {
-            const ohlcRows = await getExistingData({ type: 'ohlc', modelName: symbol });
-            console.log('Total no of ohlcRows: ', ohlcRows.length);
+            const ohlc = await getExistingData({ type: 'ohlc', modelName: symbol });
+            console.log('Total no of ohlcRows: ', ohlc.length);
 
             // technical indicators library expects the earliest data first. Thus reversing the data order. Only used for library
-            const values = ohlcRows
-                .slice()
-                .reverse()
-                .map(({ close }) => +close);
+            var ex_stamp = '';
+            var temp_array = [];
+            var elder = [];
+            
+            ohlc
+            .slice()
+            .reverse()
+            .map(( element, index ) => {
+                if (ex_stamp !== timeStamp_day(element.time)) {                        
+                    if (ex_stamp !== '') {
+                        // We are reversing the EMA values returned as it would make calculations easier later
+                        temp_array.reverse();
+                        const values = temp_array
+                            .slice()
+                            .reverse()
+                            .map(({close}) => +close);
 
-            // We are reversing the EMA values returned as it would make calculations easier later
-            const EMA = calculateEMA({ values }).reverse();
-            const MACD = calculateMACD({ values })
-                .map(({ histogram }) => histogram)
-                .reverse();
+                        const EMA = calculateEMA({ values }).reverse();
+                        const MACD = calculateMACD({ values })
+                            .map(({ histogram }) => histogram)
+                            .reverse();
 
-            const elderDataRows = [];
-            // We are using EMA array length here because the EMA period is greater than the MACD fast period [13>12].
-            // This means that  EMA.length < MACD.length
-            for (let i = 0; i < EMA.length - 1; i++) {
-                if (
-                    EMA[i] != undefined &&
-                    EMA[i + 1] != undefined &&
-                    MACD[i] != undefined &&
-                    MACD[i + 1] != undefined
-                ) {
-                    elderDataRows.push({
-                        time: ohlcRows[i].time,
-                        symbol,
-                        price: ohlcRows[i].close,
-                        color: getPriceBarColor({
-                            currentEMA: EMA[i],
-                            prevEMA: EMA[i + 1],
-                            currentMACD: MACD[i],
-                            prevMACD: MACD[i + 1],
-                        }),
-                    });
+                        const elderDataRows = [];
+                        // We are using EMA array length here because the EMA period is greater than the MACD fast period [13>12].
+                        // This means that  EMA.length < MACD.length
+                        for (let i = 0; i < EMA.length - 1; i++) {
+                            if (
+                                EMA[i] != undefined &&
+                                EMA[i + 1] != undefined &&
+                                MACD[i] != undefined &&
+                                MACD[i + 1] != undefined
+                            ) {
+                                elderDataRows.push({
+                                    time: temp_array[i].time,
+                                    symbol,
+                                    price: temp_array[i].close,
+                                    color: getPriceBarColor({
+                                        currentEMA: EMA[i],
+                                        prevEMA: EMA[i + 1],
+                                        currentMACD: MACD[i],
+                                        prevMACD: MACD[i + 1],
+                                    }),
+                                });
+                            }
+                        }
+                        elder = [...elderDataRows, ...elder];
+                        ex_stamp = timeStamp_day(element.time);
+                        temp_array = [];
+                        temp_array.push(element);
+                    } else {
+                        ex_stamp = timeStamp_day(element.time);
+                        temp_array.push(element);
+                    }
+                } else {
+                    temp_array.push(element);
+                    if (index === ohlc.length -1) {
+                        temp_array.reverse();
+                        const values = temp_array
+                            .slice()
+                            .reverse()
+                            .map(({close}) => +close);
+
+                        const EMA = calculateEMA({ values }).reverse();
+                        const MACD = calculateMACD({ values })
+                            .map(({ histogram }) => histogram)
+                            .reverse();
+
+                        const elderDataRows = [];
+                        // We are using EMA array length here because the EMA period is greater than the MACD fast period [13>12].
+                        // This means that  EMA.length < MACD.length
+                        for (let i = 0; i < EMA.length - 1; i++) {
+                            if (
+                                EMA[i] != undefined &&
+                                EMA[i + 1] != undefined &&
+                                MACD[i] != undefined &&
+                                MACD[i + 1] != undefined
+                            ) {
+                                elderDataRows.push({
+                                    time: temp_array[i].time,
+                                    symbol,
+                                    price: temp_array[i].close,
+                                    color: getPriceBarColor({
+                                        currentEMA: EMA[i],
+                                        prevEMA: EMA[i + 1],
+                                        currentMACD: MACD[i],
+                                        prevMACD: MACD[i + 1],
+                                    }),
+                                });
+                            }
+                        }
+                        elder = [...elderDataRows, ...elder];
+                    }
                 }
-            }
+            });      
 
-            return elderDataRows;
+            return elder;
         },
     },
 };
